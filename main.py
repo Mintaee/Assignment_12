e8de644e70d814a206a549cdd4bed9d9fa88db86
Mintaee <mintae020990@naver.com>
2025-05-21 18:40:38 +0900

    Fix file reading mode and correct variable overwrite in loop

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    li = open(path, 'w')
+    with open(path, 'r', encoding='utf-8') as f:
+	lines = [line.strip() for line in f.readlines()]
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
     # Preprocess unwanted characters
-    def process_file(file):
+    def process_file(line):
         if '\\' in file:
             file = file.replace('\\', '\\')
         if '/' or '"' in file:
@@ -25,7 +26,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
         english_file = process_file(english_file)
-        english_file = process_file(german_file)
+        german_file = process_file(german_file)
 
         processed_file_list.append(template_mid + english_file + template_start + german_file + template_start)
     return processed_file_list
